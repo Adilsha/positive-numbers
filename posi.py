3095361ff0d06137c850bfa105d3b685e37bf343
Adilsha <42370859+Adilsha@users.noreply.github.com>
2020-10-28 17:48:57 +0530

    Add files via upload

---
+++ posi.py
@@ -0,0 +1,10 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list2 = [12, 14, -95, 3]
+>>> for num in list2:
+	if num >= 0:
+		print([num], end = " ")
+
+		
+[12] [14] [3] 
+>>> 
